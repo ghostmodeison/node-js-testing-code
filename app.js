4ac0010cf8e8beec4b926051f1c2321895c4228b
ghostmodeison <chandanprajapati00001@gmail.com>
2025-07-09 23:12:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+const http = require('http');
+const PORT = 3000;
+const HOST = '0.0.0.0'; // 👈 Important
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello from EC2!\n');
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
+});
