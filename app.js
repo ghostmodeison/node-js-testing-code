dfaa57cd70c294f7d38991039af210b3839573df
ghostmodeison <chandanprajapati00001@gmail.com>
2025-07-12 18:40:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0'; // 👈 Important
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Jenkins!\n');
+  res.end('Jenkinshhkjkjhkjhkjhkjh!\n');
 });
 
 server.listen(PORT, HOST, () => {
