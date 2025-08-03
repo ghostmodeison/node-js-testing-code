96d8dc3bfe71d11735578733aadeb60782668e5f
ghostmodeison <chandanprajapati00001@gmail.com>
2025-08-03 19:22:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const HOST = '0.0.0.0'; // 👈 Important
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('CHANDAN PRAJAPATI --------------- JENKINS\n');
+  res.end(' --------------- JENKINS\n');
 });
 
 server.listen(PORT, HOST, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
 });
+
