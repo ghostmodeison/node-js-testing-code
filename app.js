60c6acc24a585b97f3d53dc190844a02fad75a22
ghostmodeison <chandanprajapati00001@gmail.com>
2025-08-08 16:41:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const HOST = '0.0.0.0'; // 👈 Important
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hi Mr. Chandan Prajapati');
+  res.end('Hi Mr. Chandan Prajapati-----------------------');
 });
 
 server.listen(PORT, HOST, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
 });
+
 
 
 
