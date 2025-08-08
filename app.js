3fd2fb5e3948f42c353fa1e8e9233adea7074652
Your New Name <your.email@example.com>
2025-08-08 18:58:22 +0530

    Remove node_modules from Git tracking

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0'; // 👈 Important
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Jenkins');
+  res.end('Hello Jenkins---- Chandan Prajapti');
 });
 
 server.listen(PORT, HOST, () => {
